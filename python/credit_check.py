b4b95fcba210c59d153a933b0931950adf012889
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-26 14:38:57 -0500

    Spaghetti code used to practice lessons from today. Code works

--- python/credit_check.py
+++ python/credit_check.py
@@ -6,10 +6,11 @@ def credit_check(param):
     for x in range(0, len(account_identifier), 2):  # for every other number in the list, multiply it by 2
         account_identifier[x]*=2
 
-    str_ls = [str(y) for y in account_identifier]
+    str_ls = [str(y) for y in account_identifier]   # converts each number into a string
 
     final_ls = []
 
+    # Checks if a number is double digits. If so, take the sum of their digits
     for z in str_ls:
         if(len(z) > 1):
             a = int(z[0]) + int(z[1])
@@ -17,9 +18,9 @@ def credit_check(param):
         else:
             final_ls.append(z)
             
-    final_final_ls = [int(a) for a in final_ls]
+    final_final_ls = [int(a) for a in final_ls] # convert each num_str into an integer
 
-    sum = ft.reduce(lambda agg, num : agg + num, final_final_ls)
+    sum = ft.reduce(lambda agg, num : agg + num, final_final_ls)    # take the sum of all the numbers in the list
 
     if(sum%10 == 0):
         return ("The number is valid!") 
