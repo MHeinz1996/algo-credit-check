da72f3d208df9f6679125ef92d3cbd95c7778040
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-26 14:36:38 -0500

    Horribly unoptimized, but works

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,31 @@
-def credit_check(str):
-    pass
+import functools as ft
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+def credit_check(param):
+    account_identifier = [int(x) for x in param]  # for every character in the string, convert to and int and append to new list
+   
+    for x in range(0, len(account_identifier), 2):  # for every other number in the list, multiply it by 2
+        account_identifier[x]*=2
 
+    str_ls = [str(y) for y in account_identifier]
+
+    final_ls = []
+
+    for z in str_ls:
+        if(len(z) > 1):
+            a = int(z[0]) + int(z[1])
+            final_ls.append(a)
+        else:
+            final_ls.append(z)
+            
+    final_final_ls = [int(a) for a in final_ls]
+
+    sum = ft.reduce(lambda agg, num : agg + num, final_final_ls)
+
+    if(sum%10 == 0):
+        return ("The number is valid!") 
+    else:
+        return("The number is invalid!")
+            
+
+
+# print(credit_check('5541808923795240'))
