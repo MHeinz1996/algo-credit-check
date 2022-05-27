5509116a790fdd28009eb42333cb4687b525ee13
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-27 12:45:46 -0500

    Added comments for clarity

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,8 +1,12 @@
 exports.creditCheck = function(param) {
 
-    accountIdentifier = param.split('');
+    accountIdentifier = param.split('');    // Split the string into an array of numbers
     summedDigitsOverTen = [];
     count = 0;
+
+    // Loops through array. Every other number is doubled. If the doubled number is a double digit,
+    // replace it with the sum of its digits instead. push all of those numbers to a new array:
+    // summedDigitsOverTen.
     for(let n of accountIdentifier) {
         if(count%2 == 0){
             if(String(((+n)*2)).length > 1) {
@@ -17,6 +21,7 @@ exports.creditCheck = function(param) {
         count++;
     }
 
+    // Using the reduce, we can get the sum of all of the numbers in the array
     let sum = summedDigitsOverTen.reduce((agg, num) => agg + num);
 
     if(sum%10 == 0) {
@@ -25,5 +30,3 @@ exports.creditCheck = function(param) {
         return "The number is invalid!"
     }
 }
-
-// console.log(exports.creditCheck('5541808923795240'));
